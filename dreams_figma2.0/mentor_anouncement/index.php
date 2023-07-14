779f3e1a9e24def9fc93f62cf3018f122e68b63b
Meenakshi S A <meenusa2003@gmail.com>
2023-07-14 13:48:57 +0530

    Dude

--- dreams_figma2.0/mentor_anouncement/index.php
+++ dreams_figma2.0/mentor_anouncement/index.php
@@ -6,10 +6,11 @@ include "./config.php";
         $title = $_POST['title'];
         $deadline = $_POST['deadline'];
         $description = $_POST['description'];
+        $selectedyear= $_POST['Yearlist'];
     }
 
 
-    $sql = "INSERT INTO announcement (title, deadline, description) VALUES ('$title', '$deadline', '$description')";
+    $sql = "INSERT INTO announcement (title,Yearlist, deadline, description) VALUES ('$title','$selectedyear', '$deadline', '$description')";
   
     $rs = mysqli_query($conn, $sql);
     header("Location: ./index.html");
