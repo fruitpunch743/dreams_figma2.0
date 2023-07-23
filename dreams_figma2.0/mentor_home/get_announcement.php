24b0c95f6e6dceefc14e4b04191d57c40c4c2b3d
Meenakshi S A <meenusa2003@gmail.com>
2023-07-23 20:23:53 +0530

    mentor

--- dreams_figma2.0/mentor_home/get_announcement.php
+++ dreams_figma2.0/mentor_home/get_announcement.php
@@ -1,21 +1,22 @@
 <?php
     include "./config.php";
 
-    $sql = "SELECT * FROM announcement";
+    $sql = "SELECT * FROM announcement order by  t_id desc limit 0,4";
     $result = $conn->query($sql);
     // Display names in a select tag
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            ?><div class="an-card">
+            ?><div class="details">
                 <p><?php echo $row["title"]; ?></p>
-                <h1><?php echo $row["deadline"]; ?></h1>
-                <h3><?php echo $row["description"]; ?></h3>
-                <h2>Chatper Coordinator</h2>
-                <br><br>
+                <h1><?php echo date("d-m-Y",strtotime($row["deadline"])); ?></h1><br>
+                <h3><?php echo $row["description"]; ?></h3><br>
+                <h2>Chapter Coordinator</h2>
+                <hr style="border-style: inset; border-width: 3px; width:100%; text-align:left; margin-left:0">
+                
             <?php
         }
-        ?> </div><br> <?php
+        ?></div><br><?php
     } else {
-    echo "No annoouncements for now.";
+    echo "No announcements for now.";
     }
 ?>
